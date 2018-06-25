8a7412ca821af2b9db91cc4fdf8619090e55a009
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2018-06-25 15:31:55 +0700

    Add connection model

--- src/util/secrets.ts
+++ src/util/secrets.ts
@@ -16,8 +16,14 @@ export const ENVIRONMENT = process.env.NODE_ENV || "dev";
 const prod = ENVIRONMENT === "production"; // Anything else is treated as 'dev'
 
 export const SESSION_SECRET = process.env.SESSION_SECRET;
+export const JWT_SECRET = process.env.JWT_SECRET;
 
 if (!SESSION_SECRET) {
     logger.error("No client secret. Set SESSION_SECRET environment variable.");
+    process.exit(1);
+}
+
+if (!JWT_SECRET) {
+    logger.error("No json web token secret. Set JWT_SECRET environment variable.");
     process.exit(1);
 }
