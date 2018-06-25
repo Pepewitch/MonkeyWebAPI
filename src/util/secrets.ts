34b1f6aa02503ae5cfc0d6d26c881186072673aa
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2018-06-25 11:27:44 +0700

    Add package-lock

--- src/util/secrets.ts
+++ src/util/secrets.ts
@@ -3,10 +3,14 @@ import { existsSync } from "fs";
 import logger from "./logger";
 
 if (!existsSync(".env")) {
-    console.warn(".env file not found. Create one using .env.example");
+    logger.warn(".env file not found. Create one using .env.example");
 }
 
 dotenv.config();
+
+if (!process.env.NODE_ENV) {
+    logger.info("Node environment not found use \"dev\" environment as default");
+}
 
 export const ENVIRONMENT = process.env.NODE_ENV || "dev";
 const prod = ENVIRONMENT === "production"; // Anything else is treated as 'dev'
