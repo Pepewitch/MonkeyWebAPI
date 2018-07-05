a7ec16464070a489443d46dd5f7087be1293eb81
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2018-07-05 11:42:50 +0700

    Add quarter method

--- src/controllers/router/v1/quarter.ts
+++ src/controllers/router/v1/quarter.ts
@@ -1,13 +1,16 @@
 import { Router } from "express";
-import { body } from "express-validator/check";
-import { QuarterType } from "../../../models/v1/quarter";
+import { body, oneOf } from "express-validator/check";
+import { Observable } from "rxjs";
+import { IQuarterModel, QuarterType } from "../../../models/v1/quarter";
+import { UserPosition } from "../../../models/v1/users";
 import { Quarter } from "../../../repositories/Quarter";
-import { completionHandler, validateRequest } from "../util/requestValidator";
+import { authenticateRequest, authenticateRequestWithPosition, completionHandler, validateRequest } from "../util/requestValidator";
 
 export const router = Router();
 
 router.post(
     "/add",
+    authenticateRequestWithPosition(UserPosition.admin, UserPosition.dev, UserPosition.mel),
     body("quarterID").isInt(),
     body("quarterName").isString(),
     body("quarterType").isIn(Object.keys(QuarterType)),
@@ -21,6 +24,68 @@ router.post(
             req.body.quarterType,
             req.body.startDate,
             req.body.endDate,
+        ).subscribe(completionHandler(res));
+    },
+);
+
+router.post(
+    "/default",
+    authenticateRequest,
+    body("summer").isBoolean().optional(),
+    validateRequest,
+    (req, res) => {
+        let observable: Observable<IQuarterModel | null>;
+        if (req.body.summer === true) {
+            observable = Quarter.getInstance().defaultQuarter(QuarterType.summer);
+        } else {
+            observable = Quarter.getInstance().defaultQuarter();
+        }
+        observable.subscribe(
+            (quarter) => res.status(200).send({ quarter }),
+            (error) => res.status(500).send({ error }),
+        );
+    },
+);
+
+router.post(
+    "/list",
+    authenticateRequest,
+    (_, res) => {
+        Quarter.getInstance().list().subscribe(
+            (quarters) => res.status(200).send({ quarters }),
+            (error) => res.status(500).send({ error }),
+        );
+    },
+);
+
+router.post(
+    "/delete",
+    authenticateRequestWithPosition(UserPosition.admin, UserPosition.dev, UserPosition.mel),
+    body("quarterID").isInt(),
+    validateRequest,
+    (req, res) => {
+        Quarter.getInstance().delete(req.body.quarterID).subscribe(completionHandler(res));
+    },
+);
+
+router.post(
+    "/edit",
+    authenticateRequestWithPosition(UserPosition.admin, UserPosition.dev, UserPosition.mel),
+    body("quarterID").isInt(),
+    oneOf([
+        body("quarterName").isString(),
+        body("startDate").isISO8601(),
+        body("endDate").isISO8601(),
+    ]),
+    validateRequest,
+    (req, res) => {
+        Quarter.getInstance().edit(
+            req.body.quarterID,
+            {
+                EndDate: req.body.endDate,
+                QuarterName: req.body.quarterName,
+                StartDate: req.body.startDate,
+            },
         ).subscribe(completionHandler(res));
     },
 );
--- src/repositories/Quarter.ts
+++ src/repositories/Quarter.ts
@@ -1,4 +1,5 @@
 import { from, Observable } from "rxjs";
+import { map } from "rxjs/operators";
 import Sequelize from "sequelize";
 import { Connection } from "../models/Connection";
 import { IQuarterModel, QuarterInstance, quarterModel, QuarterType } from "../models/v1/quarter";
@@ -29,5 +30,46 @@ export class Quarter {
         EndDate: Date,
     ): Observable<IQuarterModel> {
         return from(this.quarterModel.create({ ID, QuarterName, QuarterType, StartDate, EndDate }));
+    }
+
+    public defaultQuarter(
+        type = QuarterType.normal,
+    ): Observable<IQuarterModel | null> {
+        return Connection.getInstance().query<IQuarterModel>(
+            `SELECT * FROM Quarter WHERE StartDate < GETDATE() AND EndDate > GETDATE() AND Type = '${type}'`,
+            { raw: true },
+        ).pipe(
+            map((result) => result.length === 0 ? null : result[0]),
+        );
+    }
+
+    public list(): Observable<IQuarterModel[]> {
+        return from(this.quarterModel.findAll({ raw: true }));
+    }
+
+    public delete(
+        ID: number,
+    ): Observable<number> {
+        return from(this.quarterModel.destroy({ where: { ID } }));
+    }
+
+    public edit(
+        ID: number,
+        value: Partial<IQuarterModel>,
+    ): Observable<number> {
+        let updateValue = {} as Partial<IQuarterModel>;
+        if (value.StartDate) {
+            updateValue = { ...updateValue, StartDate: value.StartDate };
+        }
+        if (value.EndDate) {
+            updateValue = { ...updateValue, EndDate: value.EndDate };
+        }
+        if (value.QuarterName) {
+            updateValue = { ...updateValue, QuarterName: value.QuarterName };
+        }
+        return from(this.quarterModel.update(updateValue, { where: { ID } }))
+            .pipe(
+                map((quarter) => quarter[0]),
+        );
     }
 }
